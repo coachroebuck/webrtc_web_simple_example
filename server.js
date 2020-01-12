0513d5704d2eddf454006890ddab38b5775748a5
Coach Roebuck <mroebuck@coolcooboo.com>
2020-01-11 21:47:52 -0700

    Forget about the certificate...

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const credentials = require('./credentials');
+// const credentials = require('./credentials');
 const express = require('express');
 const app = express();
 let broadcaster;
 let server;
 let port;
-if (credentials.key && credentials.cert) {
-  const https = require('https');
-  server = https.createServer(credentials, app);
-  port = 443;
-} else {
+// if (credentials.key && credentials.cert) {
+//   const https = require('https');
+//   server = https.createServer(credentials, app);
+//   port = 443;
+// } else {
   const http = require('http');
   server = http.createServer(app);
   port = 3000;
-}
+// }
 const io = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
 io.sockets.on('error', e => console.log(e));
